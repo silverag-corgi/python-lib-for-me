03cce0620d2ea61968ca5f57bc106702c3acb615
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-07 00:10:03 +0900

    fix: #1 ライブラリのインストールエラーを修正
    setup.pyで引数として設定していた情報をsetup.cfgに移動

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_packages
-import python_lib_for_me
+from setuptools import setup
 
-
-setup(
-    name='python-lib-for-me',
-    version=python_lib_for_me.__version__,
-    # description='',
-    # long_description='',
-    # url='',
-    # author='',
-    # author_email='',
-    # license='',
-    # classifiers='',
-    # keywords='',
-    # project_urls={},
-    packages=find_packages(),
-    install_requires=['python-dateutil'],
-    python_requires='>=3.10',
-)
+setup()
