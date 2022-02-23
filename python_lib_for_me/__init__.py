7c3c17bbf79bfc334aa8cd38ce3275883622fd70
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-23 23:17:22 +0900

    build: パッケージ管理ツールを変更(Pipenv=>Poetry)

--- python_lib_for_me/__init__.py
+++
@@ -1,6 +0,0 @@
-from python_lib_for_me.date import *
-from python_lib_for_me.exception import *
-from python_lib_for_me.logger import *
-from python_lib_for_me.proc_time import *
-
-__version__ = '0.0.1'
