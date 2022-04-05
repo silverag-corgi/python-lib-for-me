fcb49b9a275547cd06ed7b488293a4d7032804a3
Rem_ <black.white.red.are.the.best@gmail.com>
2022-04-06 00:04:48 +0900

    [c_add] コード追加
    - ファイルモジュール

--- src/python_lib_for_me/__init__.py
+++ src/python_lib_for_me/__init__.py
@@ -1,6 +1,7 @@
 from .arg_parser import *
 from .date import *
 from .exception import *
+from .file import *
 from .list import *
 from .logger import *
 from .proc_time import *
---
+++ src/python_lib_for_me/file.py
@@ -0,0 +1,11 @@
+'''
+ファイルモジュール
+'''
+
+import os
+
+
+def generate_file_name(file_path: str) -> str:
+    '''ファイル名生成'''
+    file_name: str = os.path.splitext(os.path.basename(file_path))[0]
+    return file_name
