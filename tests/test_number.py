524ba117b3af4ac3881606d4fa9ed19a51fbe2d2
Rem_ <black.white.red.are.the.best@gmail.com>
2022-04-11 00:33:39 +0900

    [c_add] コード追加
    - 数値モジュール

---
+++ tests/test_number.py
@@ -0,0 +1,27 @@
+from decimal import Decimal
+
+import python_lib_for_me as pyl
+
+
+def test_round_half_up_01() -> None:
+    actual_values: Decimal = pyl.round_half_up(1.234, 2)
+    expected_values: Decimal = Decimal('1.23')
+    assert actual_values == expected_values
+
+
+def test_round_half_up_02() -> None:
+    actual_values: Decimal = pyl.round_half_up(1.235, 2)
+    expected_values: Decimal = Decimal('1.24')
+    assert actual_values == expected_values
+
+
+def test_round_half_up_03() -> None:
+    actual_values: Decimal = pyl.round_half_up(Decimal('1.234'), 2)
+    expected_values: Decimal = Decimal('1.23')
+    assert actual_values == expected_values
+
+
+def test_round_half_up_04() -> None:
+    actual_values: Decimal = pyl.round_half_up(Decimal('1.235'), 2)
+    expected_values: Decimal = Decimal('1.24')
+    assert actual_values == expected_values
